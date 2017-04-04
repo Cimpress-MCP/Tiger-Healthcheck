bce41dca50792c7ee82a580d21e2803ff041986a
Christopher Osborn <cosborn@cimpress.com>
2017-04-04 15:23:44 -0400

    Correctly Generate Documentation for Test
    [I promise]

--- src/Test.cs
+++ src/Test.cs
@@ -1,16 +1,19 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using JetBrains.Annotations;
 using Newtonsoft.Json;
 using Swashbuckle.AspNetCore.SwaggerGen;
 using static JetBrains.Annotations.ImplicitUseTargetFlags;
+using static Newtonsoft.Json.DefaultValueHandling;
 using static Newtonsoft.Json.Required;
+using static Tiger.Healthcheck.State;
 
 namespace Tiger.Healthcheck
 {
     /// <summary>Represents a status test.</summary>
     [SwaggerSchemaFilter(typeof(TestSchemaFilter))]
     [UsedImplicitly(Members)]
-    public abstract class Test
+    public sealed class Test
     {
         /// <summary>Initializes a new instance of the <see cref="Test"/> class.</summary>
         /// <param name="duration">The time taken to run the test.</param>
@@ -27,17 +30,37 @@ namespace Tiger.Healthcheck
             TestedAt = testedAt;
         }
 
+        /// <summary>Initializes a new instance of the <see cref="Test"/> class.</summary>
+        /// <param name="duration">The time taken to run the test.</param>
+        /// <param name="testedAt">The date and time at which this test was executed.</param>
+        /// <param name="error">A description of the test failure.</param>
+        /// <exception cref="ArgumentOutOfRangeException"><paramref name="duration"/> is negative.</exception>
+        internal Test(TimeSpan duration, DateTimeOffset testedAt, [NotNull] string error)
+            : this(duration, testedAt)
+        {
+            Error = error ?? throw new ArgumentNullException(nameof(error));
+        }
+
         /// <summary>Gets the number of milliseconds taken to run this test.</summary>
         [JsonProperty("duration_millis", Required = Always)]
+        [Required]
         public double Duration { get; }
 
         /// <summary>Gets the final state of this test.</summary>
         [JsonProperty(Required = Always)]
-        public abstract State Result { get; }
+        [Required]
+        public State Result => Error != null
+            ? Passed
+            : Failed;
 
         /// <summary>Gets the time at which this test was executed.</summary>
         [JsonProperty("tested_at", Required = Always)]
+        [Required]
         public DateTimeOffset TestedAt { get; }
+
+        /// <summary>Gets a description of any error conditions.</summary>
+        [JsonProperty(DefaultValueHandling = Ignore)]
+        public string Error { get; }
 
         /// <summary>Creates a passed test.</summary>
         /// <param name="duration">The time taken to run the test.</param>
@@ -46,7 +69,7 @@ namespace Tiger.Healthcheck
         /// <returns>A passed test.</returns>
         [NotNull]
         public static Test Pass(TimeSpan duration, DateTimeOffset testedAt) =>
-            new Passed(duration, testedAt);
+            new Test(duration, testedAt);
 
         /// <summary>Creates a failed test.</summary>
         /// <param name="duration">The time taken to run the test.</param>
@@ -57,51 +80,6 @@ namespace Tiger.Healthcheck
         /// <returns>A failed test.</returns>
         [NotNull]
         public static Test Fail(TimeSpan duration, DateTimeOffset testedAt, [NotNull] string error) =>
-            new Failed(duration, testedAt, error);
-
-        /// <summary>Represents a passed status test.</summary>
-        sealed class Passed
-            : Test
-        {
-            /// <summary>
-            /// Initializes a new instance of the <see cref="Passed"/> class.
-            /// </summary>
-            /// <param name="duration">The time taken to run the test.</param>
-            /// <param name="testedAt">The date and time at which this test was executed.</param>
-            /// <exception cref="ArgumentOutOfRangeException"><paramref name="duration"/> is negative.</exception>
-            public Passed(TimeSpan duration, DateTimeOffset testedAt)
-                : base(duration, testedAt)
-            {
-            }
-
-            /// <inheritdoc/>
-            public override State Result => State.Passed;
-        }
-
-        /// <summary>Represents a failed status test.</summary>
-        sealed class Failed
-            : Test
-        {
-            /// <summary>
-            /// Initializes a new instance of the <see cref="Failed"/> class.
-            /// </summary>
-            /// <param name="duration">The time taken to run the test.</param>
-            /// <param name="testedAt">The date and time at which this test was executed.</param>
-            /// <param name="error">A description of the error that caused this test to fail.</param>
-            /// <exception cref="ArgumentOutOfRangeException"><paramref name="duration"/> is negative.</exception>
-            /// <exception cref="ArgumentNullException"><paramref name="error"/> is <see langword="null"/>.</exception>
-            public Failed(TimeSpan duration, DateTimeOffset testedAt, [NotNull] string error)
-                : base(duration, testedAt)
-            {
-                Error = error ?? throw new ArgumentNullException(nameof(error));
-            }
-
-            /// <inheritdoc/>
-            public override State Result => State.Failed;
-
-            /// <summary>Gets a description of any error conditions.</summary>
-            [JsonProperty(Required = Always)]
-            public string Error { get; }
-        }
+            new Test(duration, testedAt, error);
     }
 }
